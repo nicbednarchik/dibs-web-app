05ab2ebfc5b64e8956e9dbdc6baeff99bfda6305
nicbednarchik <nikabednarchik@gmail.com>
2025-09-10 22:12:10 -0700

    got profile picture working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   # ========== Devise (auth) ==========
   # Exposes sign up, log in, log out, edit profile, etc.
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   # ========== Homepage ==========
   root "clothes#index"
